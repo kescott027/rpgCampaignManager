4f1174cb2142bbe6d0011243798c641300024f86
Kyle Scott <kylescott@rawintelligence.org>
2025-05-05 01:40:02 -0500

    fixed reference api_file_routes to correctly reference localstore name

--- src/backend/api_service.py
+++ src/backend/api_service.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .api_drive_routes import router as drive_router
-from .api_file_routes import router as file_router
+from .api_localstore_routes import router as file_router
 from .api_gpt_routes import router as gpt_router
 from .api_obs_routes import router as obs_router
 from .api_security_routes import router as security_router
